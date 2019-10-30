34641d59369afaf07108b5006ada788aac4ef82a
Pete Heyes <peteheyes@Leighs-MacBook-Pro-2.local>
2019-10-30 12:57:07 +0000

    removed version number from url string

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ import { Basket } from './Basket';
 import { Tabs, Tab, Dropdown } from 'react-bootstrap';
 import { ProductsAlert } from './ProductsAlert';
 import { GetProducts } from './GetProducts';
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost'
+import  { ApolloProvider } from 'react-apollo';
+// import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+
+const httpLink = createHttpLink({
+  uri: 'https://petes-store-test-123.myshopify.com/api/graphql',
+})
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    'X-Shopify-Storefront-Access-Token': '3e73237a41f3dd632213a7b22dc8fe5d'
+  }
+}))
+
+export const client = new ApolloClient({
+  link: middlewareLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
 
 function App(props) {
-  const client = new ApolloClient({
-    uri: "https://petes-store-test-123.myshopify.com/api/2019-07/graphql"
-  })
+
+
   return (
-    <div>
+    <div> 
       <ApolloProvider client={client}>
         <AppContext>
           <Tabs defaultActiveKey="Products" id="uncontrolled-tab-example">
--- src/GetProducts.js
+++ src/GetProducts.js
@@ -1,42 +1,46 @@
 import React, { useContext, useEffect } from 'react'
 import {Button, Card, Table } from 'react-bootstrap'
 import { MyContext } from './AppContext'
-import ApolloClient from 'apollo-boost'
-import { useQuery } from 'react-apollo'
+// import ApolloClient from 'apollo-boost'
 import gql from 'graphql-tag'
+import { client } from './App';
+import { useQuery } from 'react-apollo';
 
-const GETPRODUCTSFROMSHOPIFY = gql`
-query getProducts {
-  products(first: 10) {
-    edges {
-      node {
-        id
-        title
-        priceRange {
-          maxVariantPrice {
-            amount
-            currencyCode
-          }
-        }
-        productType
-        images (first: 1) {
-          edges {
-            node {
-              originalSrc
-            }
-          }
-        }
-      }
-    }
-  }
-}`
+// const GETPRODUCTSFROMSHOPIFY = gql`{}`
+// `
+// query getProducts {
+//   products(first: 10) {
+//     edges {
+//       node {
+//         id
+//         title
+//         priceRange {
+//           maxVariantPrice {
+//             amount
+//             currencyCode
+//           }
+//         }
+//         productType
+//         images (first: 1) {
+//           edges {
+//             node {
+//               originalSrc
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }`
 
 
 const GetProducts = (props) => {
-  const [dispatch, state, peteState, setPeteState] = useContext(MyContext)
-  const { loading, error, data } = useQuery(GETPRODUCTSFROMSHOPIFY)
-  console.log(error)
+  const {data} = useQuery(gql`{shop{name}}`)
   console.log(data)
+  // const [dispatch, state, peteState, setPeteState] = useContext(MyContext)
+  // const { loading, error, data } = useQuery(gql`{shop{name}}`)
+  // console.log({loading, data, error})
+  // console.log(data)
     // const getMeProducts = async () => {
     //     const info = await axios.get('http://localhost:3003/products')
     //     const getProducts = {
@@ -46,6 +50,7 @@ const GetProducts = (props) => {
     //     //dispatch the result of getMeProducts, which is the obj getProducts, not the whole funciton!!!!
     //     dispatch(getProducts)
     //   }
+    console.log(client)
     return (
       <Button
       onClick={()=>{}}>
